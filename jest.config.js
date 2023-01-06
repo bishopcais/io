651b570d34d5a3fe8e8655aba09050b049259a47
Matthew Peveler <matt.peveler@gmail.com>
2023-01-05 20:52:37 -0500

    Update jest to latest
    Signed-off-by: Matthew Peveler <matt.peveler@gmail.com>

--- jest.config.js
+++ jest.config.js
@@ -3,23 +3,27 @@ const { lstatSync, readdirSync } = require('fs');
 // get listing of packages in the mono repo
 const basePath = path.resolve(__dirname, 'packages');
 const packages = readdirSync(basePath).filter(name => {
-   return lstatSync(path.join(basePath, name)).isDirectory();
+  return lstatSync(path.join(basePath, name)).isDirectory();
 });
 
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   collectCoverage: true,
-  collectCoverageFrom: ['pakcages/*/src/**/*.{ts,tsx}', '!tests/'],
+  collectCoverageFrom: [
+    'packages/io/src/**/*.{ts,tsx}',
+    '!tests/',
+  ],
   coverageReporters: ['text'],
   moduleNameMapper: {
-     ...packages.reduce(
-        (acc, name) => ({
-           ...acc,
-           [`@cisl/${name}(.*)$`]:
-           `<rootDir>/packages/./${name}/src/$1`,
+    ...packages.reduce(
+      (acc, name) => ({
+        ...acc,
+        [`@cisl/${name}(.*)$`]:
+        `<rootDir>/packages/./${name}/src/$1`,
       }),
-    {},
-   ),
+      {},
+    ),
   },
+  modulePathIgnorePatterns: ['<rootDir>/packages/.*/dist/'],
 };
